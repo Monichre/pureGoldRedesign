0a91a523557a01c21a130067b47433cda67e5457
Liam Ellis <liamhellis@gmail.com>
2016-10-23 17:42:51 -0400

    solid progress on redesign, almost entirely responsive

--- js/main.js
+++ js/main.js
@@ -1,248 +1,56 @@
-
-window.onload = function() {
+$(document).ready(function(){
 	var
-		$logo = $('#logo'),
-		$camera = $('#camera'),
-		$content = $('#landing'),
-		$collabLink =$('.collab'),
-		$collab = $('#collab'),
-		$mobile = $('#mobile'),
-		//hover items //
-		$reviews = $('.review'),
-		$arrowBlurb = $('.arrow-blurb'),
-		$profile = $('#profile'),
-		$about = $('#about'),
-		$hoverItems = [$reviews, $arrowBlurb],
+		$header = $('#intro header'),
+		$intro = $('#intro'),
 
-		$desktopNav = $('#desktop-nav'),
-		$mobileCamera = $('#mobile-camera'),
-		$container = $('#container'),
-		$accordion = $('.accordion'),
-		$panel = $('.panel'),
-		// video slider variables //
-		$lexyImg = $('#lexy'),
-		$weddingImg = $('#wedding'),
-		$mazatlanImg = $('#mazatlan'),
-		$reelImg = $('#reel'),
-		$videoTitle = $('#video-title'),
-		$lexyTitle= $('<p class = "vid-title-p">Lexy Panterra</p>'),
-		$weddingTitle= $('<p class = "vid-title-p">Wedding Reel</p>'),
-		$mazatlanTitle=$('<p class = "vid-title-p">Mazatlan 2016</p>'),
-		$reelTitle=$('<p class = "vid-title-p">Pure Gold Reel</p>'),
-		$imgArray = [$lexyImg, $mazatlanImg, $reelImg, $weddingImg],
-		$titleArray = [$lexyTitle, $mazatlanTitle, $reelTitle, $weddingTitle],
+	  scroll = $(window).scrollTop(),
 		mqMobile = window.matchMedia('(max-width: 768px)'),
 		mqTablet = window.matchMedia('(min-width: 768px) and (max-width: 1130px)'),
 		mqMac = window.matchMedia('(min-width: 1130px)');
 
-		//-->functions:
-		function videoDisplay(){
-			$container.show("slide", {direction: "right"}, 1000);
-		}
 
-		function contentRemove(){
-			$content.fadeOut(1000);
-			$collab.fadeOut(1000);
-			$container.fadeOut(1000);
-		}
-		function hoverItemEffects(){
-			$hoverItems.forEach(function(elem){
-				elem.fadeOut(500);
-			});
-			$profile.animate({
-				opacity: 0
-			}, 1000);
-			$about.animate({
-				opacity: 0
-			}, 1000);
 
-		}
-		function videoTitleShow(elem, elem2){
-			elem.mouseenter(function(){
-				$videoTitle.fadeIn(500);
-				$videoTitle.append(elem2);
-			}).mouseleave(function(){
-				$videoTitle.fadeOut(500);
-				elem2.remove();
-			});
-		}
 
 	// Media Query Js //
 	if (mqMobile.matches) {
-		console.log("im in mobile");
-		$collabLink.click(function(){
-			contentRemove();
-			$mobile.fadeIn(1000);
-		});
-		$accordion.click(function(){
-			console.log($(this).siblings('section'));
-			$(this).next($panel).slideToggle(500);
-			$(this).siblings('section').next('.panel').slideUp(500);
-		});
-		$('.media').click(function(){
-			$('.mobile-videos').slideToggle(500);
-			$('#mobile-videos').siblings('section').next('.panel').slideUp(500);
-		});
+
 	} else if (mqTablet.matches){
-		$camera.click(function(){
-			contentRemove();
-			setTimeout(function(){
-				videoDisplay();
-			}, 1000);
-		});
-		$collabLink.click(function(){
-			contentRemove();
-			setTimeout(function(){
-				$collab.fadeIn(2000);
-			}, 1000);
-		});
-		$('.media').click(function(){
-			contentRemove();
-			setTimeout(function(){
-				videoDisplay();
-			}, 1000);
-		});
-		$imgArray.forEach(function(img){
-			if(img.attr('id') === "lexy"){
-				videoTitleShow(img, $lexyTitle);
-			}
-			if(img.attr('id') === "mazatlan"){
-				videoTitleShow(img, $mazatlanTitle);
-			}
-			if(img.attr('id') === "reel"){
-				videoTitleShow(img, $reelTitle);
-			}
-			if(img.attr('id') === "wedding"){
-				videoTitleShow(img, $weddingTitle);
-			}
-		});
-		$('.arrow').click(function(){
-			hoverItemEffects();
-			$('.arrow-blurb').fadeIn(1000);
-		});
-		//review hover function
-		$('.reviews').click(function(){
-			hoverItemEffects();
-			$reviews.fadeIn(1000);
-			$('.review1').animate({
-				left: '60px'
-			}, 2000);
-			$('.review2').animate({
-				right: '60px'
-			}, 2000);
-			$('.review3').animate({
-				left: '60px'
-			}, 2000);
-			$('.review4').animate({
-				right: '60px'
-			}, 2000);
-		});
 
-		//profile hover function
-		$('.profile').click(function(){
-			hoverItemEffects();
-			$profile.animate({
-				opacity: 1
-			}, 1000);
-		});
-		$('.about').click(function(){
-			hoverItemEffects();
-			$about.animate({
-				opacity: 1
-			}, 1000);
-		});
-	} else if (mqMac.matches){ // --> MACBOOK jquery
-		// -->CLICK FUNCTIONS -->
-		console.log('im in macbook');
-		$camera.click(function(){
-			console.log('im fucking clicking');
-			contentRemove();
-			setTimeout(function(){
-				videoDisplay();
-			}, 1000);
-		});
-		$collabLink.click(function(){
-			contentRemove();
-			setTimeout(function(){
-				$collab.fadeIn(2000);
-			}, 1000);
-		});
-		$('.media').click(function(){
-			contentRemove();
-			setTimeout(function(){
-				videoDisplay();
-			}, 1000);
-		});
-		$imgArray.forEach(function(img){
-			if(img.attr('id') === "lexy"){
-				videoTitleShow(img, $lexyTitle);
-			}
-			if(img.attr('id') === "mazatlan"){
-				videoTitleShow(img, $mazatlanTitle);
-			}
-			if(img.attr('id') === "reel"){
-				videoTitleShow(img, $reelTitle);
-			}
-			if(img.attr('id') === "wedding"){
-				videoTitleShow(img, $weddingTitle);
-			}
-		});
-		$('.arrow').hover(function(){
-			$('.arrow-blurb').fadeIn(1000);
-		}, function(){
-			$('.arrow-blurb').fadeOut(1000);
-		});
-		//review hover function
-		$('.reviews').hover(function(){
-			$('.review').fadeIn(1000);
-			$('.review1').animate({
-				left: '60px'
-			}, 2000);
-			$('.review2').animate({
-				right: '60px'
-			}, 2000);
-			$('.review3').animate({
-				left: '60px'
-			}, 2000);
-			$('.review4').animate({
-				right: '60px'
-			}, 2000);
-		}, function(){
-			$('.review').fadeOut(1000);
-		});
 
-		//hexagon profile hover div function //
-		$('.center-hex').hover(function(){
-			$('.hexagon').animate({
-				margin: '20px'
-			}, 500);
-		}, function(){
-			$('.hexagon').animate({
-				margin: '15px'
-			}, 500);
-		});
 
-		//profile hover function
-		$('.profile').hover(function(){
-			$('#profile').animate({
-				opacity: 1
-			}, 1000);
-		}, function(){
-			$('#profile').animate({
-				opacity: 0
-			}, 1000);
-		});
-		$('.about').hover(function(){
-			$('#about').animate({
-				opacity: 1
-			}, 1000);
-		}, function(){
-			$('#about').animate({
-				opacity: 0
-			}, 1000);
-		});
+	} else if (mqMac.matches){
+
+
+
 
 	} else {
 	}
 
-}
+
+	$(window).scroll(function(){
+		var windowWidth = $(this).width();
+	 var windowHeight = $(this).height();
+	 var windowScrollTop = $(this).scrollTop();
+	 var $introHeight = $('#intro').height();
+
+	  // if (scroll >= 10){
+		// 	header.addClass('fixed');
+		// } else {
+		// 	header.removeClass('fixed');
+		// }
+		if (windowScrollTop > ($introHeight/2)) {
+			$('.gear section:nth-child(1)').addClass('slideInLeft').show();
+			$('.gear section:nth-child(2)').addClass('slideInRight').show();
+
+		}
+	});
+	$('.fa').click(function(){
+		var id = $(this).attr('value');
+		$('#' + id).addClass('zoomInUp').show();
+
+	});
+
+
+
+
+});
